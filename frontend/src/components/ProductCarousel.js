c2e932387121990435127f0b6de4ced88206436f
Manny Cortez Studios <emailme@mannycortez.com>
2020-11-02 14:38:33 -0800

    Edited carousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -16,7 +16,7 @@ const ProductCarousel = () => {
         dispatch(listTopProducts())
     }, [dispatch])
 return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-    <Carousel pause='hover' className='bg-light' fade='false'>
+    <Carousel pause='hover' className='bg-light' fade='true'>
         {products.map(product => (
             <Carousel.Item key={product._id}>
                 <Link to={`/product/${product._id}`}>
